28080b8b96f5ed04ccf9b3bf77ecccdcb624bb37
Matt Holmes <mholmes95@outlook.com>
2024-11-10 21:41:30 +0000

    Added js to randomise dice roll

--- dice.js
+++ dice.js
@@ -1,2 +1,19 @@
+function diceRoll() {
+const randNum = Math.floor(Math.random() * 6) + 1;
+const randDice = randNum + ".png";
+const randImg = "./Images/" + randDice;
 
+const img1 = document.querySelectorAll("img") [0];
+img1.setAttribute("src", randImg);
+
+
+const newRandNum = Math.floor(Math.random() * 6) + 1;
+const newRandDice = "./Images/" + newRandNum + ".png";
+
+const img2 = document.querySelectorAll("img") [1];
+img2.setAttribute("src", newRandDice);
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", diceRoll);
 
